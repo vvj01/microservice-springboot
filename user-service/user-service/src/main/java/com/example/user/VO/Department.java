f17f7e84bb6532cda5913389e69559dc6c4cb1b7
Vaibhav Jagzap <vaibhavjagzap10@gmail.com>
2022-07-31 21:07:46 +0100

    Added user-service code

---
+++ user-service/user-service/src/main/java/com/example/user/VO/Department.java
@@ -0,0 +1,16 @@
+package com.example.user.VO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
---
+++ user-service/user-service/src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.user.controller;
+
+import com.example.user.VO.ResponseTemplateVO;
+import com.example.user.entity.UserEntity;
+import com.example.user.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    private static final String USER_SERVICE = "getUserWithDepartment";
+    private static final String USER_SERVICE_CIRCUIT = "getUserWithDepartmentCircuit";
+    private static final String USER_SERVICE_RETRY = "getUserWithDepartmentRetry";
+
+    @PostMapping("/")
+    public UserEntity saveUser(@RequestBody UserEntity userEntity){
+
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(userEntity);
+    }
+
+    @GetMapping("/user/{id}")
+    public UserEntity getUserById(@PathVariable("id") Long userId){
+         log.info("Inside getUserById of UserController");
+        return userService.getUser(userId);
+
+    }
+
+    @GetMapping("/{id}")
+    //@CircuitBreaker(name= USER_SERVICE_CIRCUIT, fallbackMethod = "getUserWithDepartmentFallback")
+    //@Retry(name = USER_SERVICE_RETRY, fallbackMethod = "getUserWithDepartmentFallback")
+    @RateLimiter(name = USER_SERVICE)
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+
+    public ResponseTemplateVO getUserWithDepartmentFallback(Exception e){
+
+        log.warn("getUserWithDepartment failing, running with fallback method!");
+
+        return new ResponseTemplateVO();
+
+    }
+
+}
